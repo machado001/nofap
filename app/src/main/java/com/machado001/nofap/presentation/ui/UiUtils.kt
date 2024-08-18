e1484156b6d18ea57e546f5f974134d54984773d
machado001 <machadowg@gmail.com>
2024-08-18 10:55:22 -0300

    add formatted util

--- app/src/main/java/com/machado001/nofap/presentation/ui/UiUtils.kt
+++ app/src/main/java/com/machado001/nofap/presentation/ui/UiUtils.kt
@@ -1,13 +1,25 @@
 package com.machado001.nofap.presentation.ui
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import com.machado001.nofap.presentation.CountScreenUiState
+import kotlin.time.Duration
 
-fun callIntent(context: Context, state: CountScreenUiState) {
-    Intent(Intent.ACTION_SEND).apply {
-        type = "text/plain"
-        putExtra(Intent.EXTRA_TEXT, "As a gigachad, My longest streak is ${state.longestStreak}.")
-        context.startActivity(Intent.createChooser(this, ""))
-    }
+fun callIntent(context: Context, state: CountScreenUiState) = Intent(Intent.ACTION_SEND).apply {
+    type = "text/plain"
+    putExtra(Intent.EXTRA_TEXT, "As a gigachad, My longest streak is ${state.longestStreak}.")
+    context.startActivity(Intent.createChooser(this, ""))
+}
+
+
+
+@SuppressLint("DefaultLocale")
+fun Duration.formatted(): String {
+    val totalSeconds = inWholeSeconds
+    val hours = String.format("%02d", totalSeconds / (60 * 60))
+    val minutes = String.format("%02d", (totalSeconds % 3600) / 60)
+    val seconds = String.format("%02d", (totalSeconds % 60))
+
+    return "$hours:$minutes:$seconds"
 }
